3237788a732bbe3e9880c07d5f340eeb7b16b49d
FaizanKhan7 <mohamedfaizankhan1@gmail.com>
2020-04-23 22:21:31 +0530

    made seperate folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Nav from "./Nav";
+import Nav from "./Nav/Nav";
 import Home from "./Home";
-import services from "./Services";
-import About from "./About";
+import services from "./Services/Services";
+import About from "./About/About";
 import shop from "./shop";
-import Contact from "./Contact";
+import Contact from "./Contact/Contact";
 import styles from "./app.module.css";
-import Footer from "./Footer";
+import Footer from "./Footer/Footer";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
